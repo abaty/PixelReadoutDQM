6c820d4c0e824ad1ec1af863e01c5070fa1b2e98
abaty <abaty@mit.edu>
2015-10-30 19:47:13 +0100

    third

--- compileBunchMacro.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-g++ $1 -Werror -Wall -O2 -o "${1/%.C/}.exe" 
--- compilePixelMacro.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-mkdir -p pdfDir
-g++ $1 $(root-config --cflags --libs) -Werror -Wall -O2 -o "${1/%.C/}.exe" 
